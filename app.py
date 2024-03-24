bd124bdc7316a0f2ddbdfd7ee77d784dd4b07be5
Raul Jose Perez <68131394+RJP-DEV@users.noreply.github.com>
2024-03-24 00:00:23 -0700

    system10

--- app.py
+++ app.py
@@ -79,8 +79,8 @@ def main():
         response = conversation(user_question)
         message = {'system':prompt,'human':user_question,'AI':response['response']}
         st.session_state.chat_history.append(message)
-      #  st.write("Chatbot:", response['response'])
-        st.write("Chatbot:", message)
+        st.write("Chatbot:", response['response'])
+      #  st.write("Chatbot:", message)
 
 if __name__ == "__main__":
     main()
