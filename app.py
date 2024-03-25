0378a7b3521e0b044f15a9c84ca3e1865b7d3c7d
Raul Jose Perez <68131394+RJP-DEV@users.noreply.github.com>
2024-03-24 22:19:33 -0700

    tea55

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def main():
 
     
     
-    conversation = ConversationChain( PromptTemplate=promptx, llm=groq_chat , memory=memory)
+    conversation = ConversationChain( SystemMessagePromptTemplate=promptx, llm=groq_chat , memory=memory)
 
     # If the user has asked a question,
     if user_question:
