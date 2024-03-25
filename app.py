5fb2cff9879b95b1469668f2994f6e91c3cc4559
Raul Jose Perez <68131394+RJP-DEV@users.noreply.github.com>
2024-03-24 20:56:08 -0700

    tea40

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def main():
 
 
     # Initialize Groq Langchain chat object and conversation
-    groq_chat = ChatGroq( groq_api_key=groq_api_key, model_name=model  )
+    groq_chat = ChatGroq ( SystemMessagePromptTemplate=messages, groq_api_key=groq_api_key, model_name=model  )
 
     
     
