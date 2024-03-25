dd087ff3d7b07194604b3612ee826f0793d59ebc
Raul Jose Perez <68131394+RJP-DEV@users.noreply.github.com>
2024-03-24 22:49:05 -0700

    ss3

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ from langchain_groq import ChatGroq
 from langchain.prompts import SystemMessagePromptTemplate, PromptTemplate, HumanMessagePromptTemplate
 from langchain_core.prompts import ChatPromptTemplate 
 
-
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
 
          
  
@@ -18,19 +19,35 @@ client = Groq(
     api_key= "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" )
 
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Explain the importance of low latency LLMs",
-        }
-    ],
-    model="mixtral-8x7b-32768",
-)
-
-print(chat_completion.choices[0].message.content)
 
 
+class PersonalityChatBot(ChatBot):
+    def __init__(self, personality):
+        super().__init__(personality)
+        self.set_trainer(ChatterBotCorpusTrainer)
 
+        if personality == 'Friendly':
+            self.train("chatterbot.corpus.english.greetings",
+                       "chatterbot.corpus.english.conversations.greetings_and_farewells")
+        elif personality == 'Professional':
+            self.train("chatterbot.corpus.english.conversations.work")
+        else:
+            raise ValueError("Invalid personality type")
 
+def reset_chat(chatbot):
+    chatbot.storage.drop()
 
+if __name__ == "__main__":
+    personality = input("Enter the personality of the chatbot (Friendly or Professional): ")
+    chatbot = PersonalityChatBot(personality)
+
+    print("Hi, I'm your chatbot with the personality: " + personality)
+
+    while True:
+        user_input = input("\nYou: ")
+        if user_input.lower() == 'reset':
+            reset_chat(chatbot)
+            print("Chatbot has been reset.")
+            continue
+        response = chatbot.get_response(user_input)
+        print("Chatbot: " + str(response))
