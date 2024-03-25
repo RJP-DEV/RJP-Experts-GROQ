228c57ae4f34afb0915c5390085bcc973b502a91
Raul Jose Perez <68131394+RJP-DEV@users.noreply.github.com>
2024-03-24 20:09:42 -0700

    tea31

--- app.py
+++ app.py
@@ -73,8 +73,8 @@ def main():
        # response = conversation(user_question)
         response = "simple"
       #  message = {'human':user_question,'AI':response['response']}
-        st.session_state.chat_history.append(message)
-        st.write("Chatbot:", response['response'])
+      #  st.session_state.chat_history.append(message)
+      #  st.write("Chatbot:", response['response'])
         st.write("system:", message['system'])
 
 if __name__ == "__main__":
