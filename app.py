f1a5f8119d24ec2e758738b12063c984dc063982
Raul Jose Perez <68131394+RJP-DEV@users.noreply.github.com>
2024-03-27 03:45:55 -0700

    dd1

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def main():
     )
 
     # Display the Groq logo
-    spacer, col = st.columns([3, 1])  
+    spacer, col = st.columns([2, 1])  
     with col:  
         st.image('groqcloud_darkmode.png')
 
@@ -95,7 +95,7 @@ def main():
     #with col: 
     #st.subheader(":blue[The Experts!]")  
     
-    spacer, col = st.columns([3, 1])  
+    spacer, col = st.columns([1, 1])  
     with col:  
          st.latex(r''' a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} = \sum_{k=0}^{n-1} ar^k = a \left(\frac{1-r^{n}}{1-r}\right) ''')
 
