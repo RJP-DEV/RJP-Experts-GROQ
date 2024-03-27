23870da9391b45e4d5589d23bb8626ffaa2bd5cb
Raul Jose Perez <68131394+RJP-DEV@users.noreply.github.com>
2024-03-27 03:47:11 -0700

    ee1

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def main():
     #with col: 
     #st.subheader(":blue[The Experts!]")  
     
-    spacer, col = st.columns([1, 1])  
+    spacer, col = st.columns([1, 0])  
     with col:  
          st.latex(r''' a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} = \sum_{k=0}^{n-1} ar^k = a \left(\frac{1-r^{n}}{1-r}\right) ''')
 
