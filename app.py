c11a0b746851dbcdc71e95b4ad600c08a540317d
Raul Jose Perez <68131394+RJP-DEV@users.noreply.github.com>
2024-03-27 03:23:15 -0700

    aa34

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ def main():
     # The title and greeting message of the Streamlit application
     st.subheader("RJP Development Presents : ") 
 
-    spacer, col = st.columns([5, 2])  
+    spacer, col = st.columns([6, 1])  
     with col: 
         st.title(":blue[The Experts!]")  
     
