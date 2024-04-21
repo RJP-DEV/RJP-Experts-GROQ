344ad2295f12d165697a409de5b7f26a9e998a81
Raul Jose Perez <68131394+RJP-DEV@users.noreply.github.com>
2024-04-20 23:24:41 -0700

    emojis2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import random
 from PIL import Image
 
 
-def chat_with_groq(client,promptx,prompt,model):
+def chat_with_groq(client,promptx,prompt,model,temperaturex):
     """
     This function sends a chat message to the Groq API and returns the content of the response.
     It takes three parameters: the Groq client, the chat prompt, and the model to use for the chat.
@@ -13,7 +13,11 @@ def chat_with_groq(client,promptx,prompt,model):
     
     completion = client.chat.completions.create(
     model=model,
-    messages=[{"role": "system", "content": promptx }, {"role": "user", "content": prompt } ]
+    messages=[{"role": "system", "content": promptx }, {"role": "user", "content": prompt } ],
+    temperature=temperaturex,
+    top_p=1,
+    stream=True,
+    stop=None,
     )
   
     return completion.choices[0].message.content
@@ -125,6 +129,9 @@ def main():
     
     # Add customization options conversational memory length in the sidebar
     conversational_memory_length = st.sidebar.slider('Conversational memory length:', 1, 10, value = 5)
+
+    # Add customization options temperature in the sidebar
+    temperaturex = st.sidebar.slider('Temperature:', 0, 2, value = 0.5)
 
 
      # Add customization options to Select system prompts in the sidebar
