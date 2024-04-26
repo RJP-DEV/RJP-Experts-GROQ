389e957a041bcebb9f15dd6a5ef9a7b732c05734
Raul Jose Perez <68131394+RJP-DEV@users.noreply.github.com>
2024-04-26 04:14:01 -0700

    rer4

--- app.py
+++ app.py
@@ -311,8 +311,9 @@ def main():
 
        text_input_container = st.empty()
        text_input_container = st.session_state.translation
-       st.code(text_input_container.empty())
-       #text_input_container.empty()
+       text_input_container.empty()
+       st.code(text_input_container)
+       
       
             
     if Resetclicked:
