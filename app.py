13910b0f7440e7317b9003a6999365496cae86ec
Raul Jose Perez <68131394+RJP-DEV@users.noreply.github.com>
2024-04-26 02:01:32 -0700

    cc1

--- app.py
+++ app.py
@@ -303,6 +303,7 @@ def main():
 
     if st.session_state.translation:
        st.audio("translation.mp3", format="audio/mpeg",)
+       
        result_container = st.container()
        _, col2, _ = result_container.columns([1, 1, 1])
        with st.container(height=90):
@@ -310,7 +311,8 @@ def main():
             st.markdown(" ")
             st.markdown(" ")
             st.markdown(" ")
-            st.markdown(st.session_state.translation)
+            st.markdown(" ")
+            st.code(st.session_state.translation)
 
 
             
