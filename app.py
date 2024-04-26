a5719c36f3cf49ec473231a8593bf97c53d47a1d
Raul Jose Perez <68131394+RJP-DEV@users.noreply.github.com>
2024-04-26 16:18:14 -0700

    1237

--- app.py
+++ app.py
@@ -295,7 +295,7 @@ def main():
         st.session_state.translation = st.session_state.translation.replace('**', '  ')
         st.session_state.translation = st.session_state.translation.replace('*', ' ')
         st.session_state.translation = string_val + string_val + st.session_state.translation
-        st.session_state.translation = col2.st.session_state.translation
+        st.session_state.translation = st.write(st.session_state.translation)
         
         convert_text_to_mp3(st.session_state.translation, supported_languages[target_language])
         
