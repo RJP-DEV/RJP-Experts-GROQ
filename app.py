607be92979d0c659f667d8d12167c694f05785e0
Raul Jose Perez <68131394+RJP-DEV@users.noreply.github.com>
2024-05-09 23:59:17 -0700

    ss8

--- app.py
+++ app.py
@@ -37,12 +37,12 @@ def detect_source_language(client, text: str ) -> str:
     :rtype: str
     :returns: Detected language of source text 
     """
-    
+    st.write(text)
     response = client.chat.completions.create(
         model="mixtral-8x7b-32768",
         messages=[
             { "role": "system", "content": "You are a multi-language translator that only translate to english. and you answer with 1 word only and without punctuation." },
-            { "role": "user",   "content": "Which language is {text} written in? answer with 1 word only without punctuation." }
+            { "role": "user",   "content": "Which language is '{text}' written in? answer with 1 word only without punctuation." }
                  ],
         temperature=0
     )
