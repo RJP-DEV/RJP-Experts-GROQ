a1f9602f57a67270e39ffb34389659002e1a294e
Raul Jose Perez <68131394+RJP-DEV@users.noreply.github.com>
2024-05-25 00:00:20 -0700

    Update languages.py

--- languages.py
+++ languages.py
@@ -5,7 +5,7 @@ supported_languages = {
     "German": "de",
     "Greek": "el",
     "English": "en",
-    "Spanish": "it",
+    "Spanish": "lv",
     "Estonian": "et",
     "Finnish": "fi",
     "French": "fr",
